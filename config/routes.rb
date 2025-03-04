f0959fc89d7667cee7ddcdd82753c68782fc1a5d
Sifiso Maye <sifmay98@gmail.com>
2025-03-04 15:31:33 +0200

    merge

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get "/countries/:id", to: "countries#show"
-  get "/countries", to: "countries#index"
+  # get "/countries/:id", to: "countries#show"
+  # get "/countries", to: "countries#index"
 end
