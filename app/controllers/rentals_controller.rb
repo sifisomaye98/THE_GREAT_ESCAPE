a36597db3f8ad4ee76a1adbba548d43cff9dffab
Sifiso Maye <sifmay98@gmail.com>
2025-03-05 15:29:33 +0200

    merge

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,6 +2,16 @@ class RentalsController < ApplicationController
   def index
     @rentals = Rental.all
   end
+
+  def accept
+    @rental.update(status: 1)
+    redirect_to 
+
+  end
+
+  def decline
+    @rental.update(status: 2)
+    redirect_to
+  end
+
 end
-
-
