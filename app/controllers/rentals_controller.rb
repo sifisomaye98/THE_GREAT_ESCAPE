90f19b737dcf4fbd95b9c6777ec920a1a0203d63
Bongiwe Nkumane <nakhonkumane@gmail.com>
2025-03-05 17:08:26 +0200

    heroku push

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -37,13 +37,4 @@ class RentalsController < ApplicationController
   def decline
     @rental.update(status: 2)
   end
-
-  def create
-    @rentals = Rental.new(params[:id])
-    if @rental.save! # Will raise ActiveModel::ForbiddenAttributesError
-      redirect_to rental_path(@rental)
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
 end
