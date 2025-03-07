8aa6662f427de5dceafe6c832062db08468a9c6c
Sifiso Maye <sifmay98@gmail.com>
2025-03-07 12:35:18 +0200

    mergin

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -24,7 +24,7 @@ class CountriesController < ApplicationController
     @country = Country.new(country_params)
     @country.user = current_user
     if @country.save!
-      redirect_to country_path(@country)
+      redirect_to country_path(@country), alert: "country created"
     else
       render :new, status: :unprocessable_entity
     end
